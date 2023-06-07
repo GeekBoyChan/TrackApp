118f4dc33c2b673c746f15a6a3f0985eb1b91043
Chandler Ocapan <Cocapan@gmail.com>
2023-06-06 17:23:42 -0700

    Implemented feature that stops add location functionality when not on the track create screen to preserve phone battery life

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,49 +1,21 @@
 // import '../_mockLocation';
-import React, { useEffect, useState, useContext } from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
 import { Text } from 'react-native-elements';
-import { SafeAreaView, StatusBar } from 'react-native';
-import {
-  requestForegroundPermissionsAsync,
-  watchPositionAsync,
-  Accuracy,
-} from 'expo-location';
+import { withNavigationFocus } from 'react-navigation';
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext);
-  const [err, setErr] = useState(null);
-
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestForegroundPermissionsAsync();
-      await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        (location) => {
-          addLocation(location);
-        }
-      );
-      if (!granted) {
-        throw new Error('Location permission not granted');
-      }
-    } catch (e) {
-      setErr(e);
-    }
-  };
-
-  useEffect(() => {
-    startWatching();
-  }, []);
+  const [err] = useLocation(isFocused, addLocation);
 
   return (
     <SafeAreaView style={{ marginTop: StatusBar.currentHeight }}>
       <Text h2>Create a Track</Text>
       <Map />
+
       {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
@@ -51,4 +23,4 @@ const TrackCreateScreen = () => {
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
