63772878d92ba8bfa26cf610403ca387db6cc534
Chandler Ocapan <Cocapan@gmail.com>
2023-06-08 16:36:00 -0700

    Implemented api call to track server in order to save track name & locations to the server

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: 'https://8034-2601-647-6400-2600-bd70-afe5-34e8-9520.ngrok-free.app',
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
   const {
@@ -10,8 +11,8 @@ const TrackForm = () => {
     stopRecording,
     changeName,
   } = useContext(LocationContext);
+  const [saveTrack] = useSaveTrack();
 
-  console.log(locations.length);
   return (
     <>
       <Spacer>
@@ -21,11 +22,18 @@ const TrackForm = () => {
           value={name}
         />
       </Spacer>
-      {recording ? (
-        <Button title='Stop' onPress={stopRecording} />
-      ) : (
-        <Button title='Start Recording' onPress={startRecording} />
-      )}
+      <Spacer>
+        {recording ? (
+          <Button title='Stop' onPress={stopRecording} />
+        ) : (
+          <Button title='Start Recording' onPress={startRecording} />
+        )}
+      </Spacer>
+      <Spacer>
+        {!recording && locations.length ? (
+          <Button title='Save Recording' onPress={saveTrack} />
+        ) : null}
+      </Spacer>
     </>
   );
 };
