8a0bdb633fb3079bc130daa7d4f14ff3f8b9fd20
Chandler Ocapan <Cocapan@gmail.com>
2023-06-08 17:31:01 -0700

    Implemented the Track Detail screen to show the user their hike/jogging trail

--- App.js
+++ App.js
@@ -13,6 +13,26 @@ import { Provider as AuthProvider } from './src/context/AuthContext';
 import { Provider as LocationProvider } from './src/context/LocationContext';
 import { Provider as TrackProvider } from './src/context/TrackContext';
 import { setNavigator } from './src/navigationRef';
+import { FontAwesome } from '@expo/vector-icons';
+
+const trackListFlow = createStackNavigator({
+  TrackList: TrackListScreen,
+  TrackDetail: TrackDetailScreen,
+});
+
+trackListFlow.navigationOptions = {
+  title: 'Tracks',
+  tabBarIcon: <FontAwesome name='th-list' size={20} />,
+};
+
+const mainFlow = createMaterialBottomTabNavigator(
+  {
+    trackListFlow,
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen,
+  },
+  { barStyle: { backgroundColor: '#f0edf6' } }
+);
 
 const switchNavigator = createSwitchNavigator({
   ResolveAuth: ResolveAuthScreen,
@@ -20,14 +40,7 @@ const switchNavigator = createSwitchNavigator({
     Signup: SignupScreen,
     Signin: SigninScreen,
   }),
-  mainFlow: createMaterialBottomTabNavigator({
-    trackListFlow: createStackNavigator({
-      TrackList: TrackListScreen,
-      TrackDetail: TrackDetailScreen,
-    }),
-    TrackCreate: TrackCreateScreen,
-    Account: AccountScreen,
-  }),
+  mainFlow,
 });
 
 const App = createAppContainer(switchNavigator);
--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -4,18 +4,24 @@ import { Button } from 'react-native-elements';
 import { SafeAreaView, StatusBar } from 'react-native';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
+import { FontAwesome } from '@expo/vector-icons';
 
 const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
 
   return (
     <SafeAreaView style={{ marginTop: StatusBar.currentHeight }}>
-      <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+      <Text style={{ fontSize: 48 }}>Account Screen</Text>
       <Spacer>
         <Button title='Sign Out' onPress={signout} />
       </Spacer>
     </SafeAreaView>
   );
+};
+
+AccountScreen.navigationOptions = {
+  title: 'Account',
+  tabBarIcon: <FontAwesome name='gear' size={20} />,
 };
 
 const styles = StyleSheet.create({});
